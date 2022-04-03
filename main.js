d933f3eb46fcfef45c0fc24cd12f1acfa7c99fa2
Matheus Roichman <roichmankill@gmail.com>
2022-04-03 15:32:38 -0300

    Adicionada função que calcula a idade do usuário

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("OI!");
+function calcIdade(event) {
+  event.preventDefault();
+
+  const anoNascimento = document.getElementById("anoNascimento").value;
+  const anoAtual = new Date().getFullYear();
+  const idade = anoAtual - anoNascimento;
+
+  document.getElementById("paragrafoIdade").innerHTML = "Sua idade é " + idade;
+} 
